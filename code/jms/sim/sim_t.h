b4a9f5051039845ebe9ebe04f05d1486dc9306a0
Jason Stredwick <jason.stredwick@gmail.com>
2021-05-25 13:57:50 -0700

    constexpr implies inline.

--- code/jms/sim/sim_t.h
+++ code/jms/sim/sim_t.h
@@ -3,6 +3,7 @@
 
 
 #include <cfloat>
+#include <limits>
 
 
 namespace jms {
@@ -12,17 +13,19 @@ namespace sim {
 #if 1
 // 7 significant digits
 using sim_t = float;
-inline constexpr sim_t MAX_VALUE = FLT_MAX;
-inline constexpr sim_t NEG_ONE = -1.0f;
-inline constexpr sim_t ONE = 1.0f;
-inline constexpr sim_t ZERO = 0.0f;
+constexpr sim_t MAX_VALUE = std::numeric_limits<sim_t>::max();
+constexpr sim_t NEG_ONE = -1.0f;
+constexpr sim_t ONE = 1.0f;
+constexpr sim_t TWO = 2.0f;
+constexpr sim_t ZERO = 0.0f;
 #else
 // 16 significant digits
 using sim_t = double;
-inline constexpr sim_t MAX_VALUE = DBL_MAX;
-inline constexpr sim_t NEG_ONE = -1.0;
-inline constexpr sim_t ONE = 1.0;
-inline constexpr sim_t ZERO = 0.0;
+constexpr sim_t MAX_VALUE = std::numeric_limits<sim_t>::max();
+constexpr sim_t NEG_ONE = -1.0;
+constexpr sim_t ONE = 1.0;
+constexpr sim_t TWO = 2.0;
+constexpr sim_t ZERO = 0.0;
 #endif
 
 
